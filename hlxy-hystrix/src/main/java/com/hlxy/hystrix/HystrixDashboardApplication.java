f707109bda4c248c3c4bff0a12658d32d107a3ce
peach_mr@sina.com <peach_mr@sina.com>
2017-03-22 13:19:44 +0800

    fsd

---
+++ hlxy-hystrix/src/main/java/com/hlxy/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,24 @@
+package com.hlxy.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * <p>描述:</p>
+ *
+ * @author Tao xs
+ * @since 2.0
+ * <p>Created by Tao xs on 2017/3/22.</p>
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+@EnableTurbine
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
---
+++ hlxy-user/src/test/java/com/hlxy/UserApplicationTest.java
@@ -0,0 +1,20 @@
+//package com.hlxy;
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//
+///**
+// * <p>描述:</p>
+// *
+// * @author Tao xs
+// * @since 2.0
+// * <p>Created by Tao xs on 2017/3/21.</p>
+// */
+//@EnableDiscoveryClient
+//@SpringBootApplication
+//public class UserApplicationTest {
+//    public static void main(String[] args) {
+//        new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
+//    }
+//}
